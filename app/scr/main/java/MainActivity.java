d0ba8c413902f349378ea0c48952542a57a4d88b
hudaliao <hhl_0818@163.com>
2016-11-21 18:07:37 +0800

    Add files via upload

---
+++ app/scr/main/java/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hailin_hu.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
